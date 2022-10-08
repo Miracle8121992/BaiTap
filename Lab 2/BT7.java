56dac6df68c911141c05b541d254931b9035ae7e
Miracle8121992 <115023836+Miracle8121992@users.noreply.github.com>
2022-10-08 17:15:46 +0700

    Add files via upload

---
+++ Lab 2/BT7.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class BT7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số thứ nhất: ");
+        int Gio = sc.nextInt();
+
+        System.out.print("Nhập số thứ hai: ");
+        int Phut = sc.nextInt();
+
+        System.out.print("Nhập số thứ ba: ");
+        int Giay = sc.nextInt();
+
+        if (((Gio > 0) && (Gio < 23)) && ((Phut > 0) && (Phut < 59)) && ((Giay > 0) && (Giay < 59))) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+        }
+            else if ((Gio == 23) && (Giay == 59) && (Phut == 59)) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: 23:59:58");
+            System.out.println("Thời gian nhanh hơn 1 giây là: 00:00:00");
+            }
+            else if ((Giay == 59) && (Phut == 59)) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: " + Gio + ":59:58");
+            System.out.println("Thời gian nhanh hơn 1 giây là: " + (Gio + 1) + ":00:00");
+            }
+            else if (Giay == 59) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: " + Gio + ":" + Phut + ":58");
+            System.out.println("Thời gian nhanh hơn 1 giây là: " + Gio + ":" + (Phut + 1) + ":00");
+            }
+            else if ((Gio == 0) && (Giay == 0) && (Phut == 0)) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: 23:59:59");
+            System.out.println("Thời gian nhanh hơn 1 giây là: 00:00:01");
+            }
+            else if ((Giay == 0) && (Phut == 0)) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: " + (Gio - 1) + ":59:59");
+            System.out.println("Thời gian nhanh hơn 1 giây là: " + Gio + ":01:01");
+            }
+            else if (Giay == 0) {
+            System.out.println("Ba số bạn vừa nhập là thời gian hợp lệ: " + Gio + ":" + Phut + ":" + Giay);
+            System.out.println("Thời gian chậm hơn 1 giây là: " + (Gio - 1) + ":" + (Phut - 1) + ":" + "59");
+            System.out.println("Thời gian nhanh hơn 1 giây là: " + Gio + ":" + Phut + ":01");
+            }
+        else {
+            System.out.print("Ba số bạn vừa nhập không phải là thời gian hợp lệ");
+        }
+    }
+}
