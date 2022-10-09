5ef698a63f48aedb21cb4415cba0f6977651e095
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-09 13:04:26 +0700

    Update BT5.java

--- Lab 2/BT5.java
+++ Lab 2/BT5.java
@@ -4,33 +4,17 @@ public class BT5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Nhập ký tự: ");
-        char Kytu = sc.next().charAt(0);
+        System.out.print("Input character: ");
+        char Character = sc.next().charAt(0);
 
-        switch (Kytu) {
-            case 'a':
-                System.out.println("Ada");
-                break;
-            case 'b':
-                System.out.println("Basic");
-                break;
-            case 'c':
-                System.out.println("Cobol");
-                break;
-            case 'd':
-                System.out.println("Android");
-                break;
-            case 'f':
-                System.out.println("Fortran");
-                break;
-            case 'w':
-                System.out.println("Windows phone");
-                break;
-            default: {
-                System.out.println("Bạn đã nhập sai.");
-                break;
-
-            }
+        switch (Character) {
+            case 'a', 'A' -> System.out.println("Ada");
+            case 'b', 'B' -> System.out.println("Basic");
+            case 'c', 'C' -> System.out.println("Cobol");
+            case 'd', 'D' -> System.out.println("Android");
+            case 'f', 'F' -> System.out.println("Fortran");
+            case 'w', 'W' -> System.out.println("Windows phone");
+            default -> System.out.println("Invalid.");
 
         }
     }
