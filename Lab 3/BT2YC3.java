016919fbb098e49605fd0b3dde151b61a1e608a7
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-10 22:38:44 +0700

    Add files via upload

---
+++ Lab 3/BT2YC3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BT2YC3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập N: ");
+        int n = sc.nextInt();
+        int i = 1;
+        int s = 0;
+
+        while ((1 <= i) && (i <= n)) {
+            if (i % 3 == 0) {
+                if (i % 7 == 0) {
+                    s = s + 1;
+                }
+            }
+            i++;
+        }
+        System.out.print("so luong so chia het cho 3 va 7 la: " + s);
+    }
+}
