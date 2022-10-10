f380091c911e3fdae80ef11ca334b149ad6fc8b8
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-10 23:24:34 +0700

    Add files via upload

---
+++ Lab 3/BT4YC1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BT4YC1 {
+    public static void main(String[] args) {
+        //int price = (int) Math.random();
+        int price = 20;
+        int buy =0;
+        int guess = 10;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Guess a price: ");
+        buy = sc.nextInt();
+
+
+        while (buy > price) {
+            System.out.println("The real price is lower than the price you guess :");
+            System.out.print("Guess another price: ");
+            buy = sc.nextInt();
+            guess = guess - 1;
+            if (guess == 1) {
+                System.out.println("You lose!"); break;
+            }
+        }
+        while (buy < price) {
+            System.out.println("The real price is higher than the price you guess :");
+            System.out.print("Guess another price: ");
+            buy = sc.nextInt();
+            guess = guess - 1;
+            if (guess == 1) {
+                System.out.println("You lose!"); break;
+
+            }
+        }
+
+        if (buy == price) {
+                    System.out.println("Exactly!");
+                    System.out.println("Number of incorrectly guess is: " + (10 - guess));
+        }
+    }
+}
