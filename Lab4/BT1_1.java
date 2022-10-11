9baf6dc0017847b182f6801caa04b25486f36e91
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-11 21:45:12 +0700

    Add files via upload

---
+++ Lab4/BT1_1.java
@@ -0,0 +1,15 @@
+public class BT1_1 {
+    public static void main(String[] args) {
+
+        int array[] = {10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6};
+
+        for(int i = 0; i < 15; i++)
+        {
+           if (array[i] % 2 == 0) {
+           }
+           else {
+               System.out.println("cac so le trong mang là: " + array[i] + " tai vi tri " + i);
+           }
+        }
+    }
+}
