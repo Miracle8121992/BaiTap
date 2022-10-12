80473de81d4852a9b9f101fbf334a87d1b59e4a0
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-12 10:28:55 +0700

    Update BT1_1.java

--- Lab4/BT1_1.java
+++ Lab4/BT1_1.java
@@ -3,7 +3,7 @@ public class BT1_1 {
 
         int array[] = {10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6};
 
-        for(int i = 0; i < 15; i++)
+        for(int i = 0; i <= arr.length; i++)
         {
            if (array[i] % 2 == 0) {
            }
