1fd1e977f115d47b4c486bd44a199e6cc5c32ff9
Cườngg <115023836+Miracle8121992@users.noreply.github.com>
2022-10-12 10:36:03 +0700

    Update BT1_4.java

--- Lab4/BT1_4.java
+++ Lab4/BT1_4.java
@@ -2,7 +2,7 @@ public class BT1_4 {
     public static void main(String[] args) {
         int array[] = {10, 25, -4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42, -6};
 
-        for(int i = 0; i < 15; i++)
+        for(int i = 0; i < array.length; i++)
         {
             if (array[i] < 0) {
                 array[i] =0;
